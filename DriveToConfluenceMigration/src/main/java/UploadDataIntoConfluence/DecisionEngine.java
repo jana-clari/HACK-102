73e16a3b6a24fc739ae8658c1931bd09d7c567d3
hchepey-clari <hchepey@clari.com>
2022-03-15 19:49:16 +0530

    DecisionEngine Placeholder

---
+++ DriveToConfluenceMigration/src/main/java/UploadDataIntoConfluence/DecisionEngine.java
@@ -0,0 +1,21 @@
+package UploadDataIntoConfluence;
+
+import java.util.ArrayList;
+
+public class DecisionEngine {
+    public static void CreateOrUpdate(ArrayList<String> Ids) {
+        for (int i = 0; i < (long) Ids.size(); i++) {
+            // for update, call update
+            //else call create
+        }
+    }
+
+    private void Create(String pageTitle, String pageBody) {
+
+    }
+
+    private void Update(String Id, String pageTitle, String pageBody){
+
+    }
+}
+
